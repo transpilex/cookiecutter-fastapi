6c0716f29e21f83f35952f6fee1f052e11c20893
Kenil Sudani <kenilsudani.blog@gmail.com>
2026-03-19 11:25:11 +0530

    fix: used env variables to replace hardcoded database passwords

--- {{ cookiecutter.slug }}/apps/config.py
+++ {{ cookiecutter.slug }}/apps/config.py
@@ -1,7 +1,12 @@
 import os
+from pathlib import Path
+from dotenv import load_dotenv
 from pydantic import computed_field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+# Load environment variables from the .env file in the root directory
+ENV_PATH = Path(__file__).resolve().parent.parent / ".env"
+load_dotenv(dotenv_path=ENV_PATH)
 
 class AppSettings(BaseSettings):
     STATIC_URL: str = "/static"
@@ -20,11 +25,11 @@ class SQLiteSettings(DatabaseSettings):
 
 
 class MySQLSettings(DatabaseSettings):
-    MYSQL_USER: str = "username"
-    MYSQL_PASSWORD: str = "password"
+    MYSQL_USER: str
+    MYSQL_PASSWORD: str
     MYSQL_SERVER: str = "localhost"
     MYSQL_PORT: int = 5432
-    MYSQL_DB: str = "dbname"
+    MYSQL_DB: str
     MYSQL_SYNC_PREFIX: str = "mysql://"
     MYSQL_ASYNC_PREFIX: str = "mysql+aiomysql://"
     MYSQL_URL: str | None = None
@@ -38,11 +43,11 @@ class MySQLSettings(DatabaseSettings):
 
 
 class PostgresSettings(DatabaseSettings):
-    POSTGRES_USER: str = "postgres"
-    POSTGRES_PASSWORD: str = "postgres"
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
     POSTGRES_SERVER: str = "localhost"
-    POSTGRES_PORT: int = 5432
-    POSTGRES_DB: str = "postgres"
+    POSTGRES_PORT: int = 3306
+    POSTGRES_DB: str
     POSTGRES_SYNC_PREFIX: str = "postgresql://"
     POSTGRES_ASYNC_PREFIX: str = "postgresql+asyncpg://"
     POSTGRES_URL: str | None = None
@@ -58,10 +63,11 @@ class PostgresSettings(DatabaseSettings):
 class Settings(
     AppSettings,
     SQLiteSettings,
+    MySQLSettings,
     PostgresSettings
 ):
     model_config = SettingsConfigDict(
-        env_file=os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", ".env"),
+        env_file=ENV_PATH,
         env_file_encoding="utf-8",
         case_sensitive=True,
         extra="ignore",
