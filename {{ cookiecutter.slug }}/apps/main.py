1ccb2305a39f6935113ab9c95c3fd97403139068
Kenil Sudani <kenilsudani.blog@gmail.com>
2026-04-21 09:25:19 +0530

    fix: missing imports

--- {{ cookiecutter.slug }}/apps/main.py
+++ {{ cookiecutter.slug }}/apps/main.py
@@ -1,3 +1,6 @@
+from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
